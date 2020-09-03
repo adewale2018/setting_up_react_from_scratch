c698010a9d15389a553b0317c7c56326ef1df5e9
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2020-09-03 07:18:02 +0100

    adding redux to the application

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
+import { configureStore } from "./src/store";
 import App from "./src/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={configureStore()}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
